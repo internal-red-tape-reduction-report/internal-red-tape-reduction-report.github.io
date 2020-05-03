2a10d6e6dad37d7af3663c8513230c808dc48f93
Sean Boots <sean.boots@gmail.com>
2020-05-03 16:30:21 -0700

    Updates

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@
 
 var app = app || {};
 
+app.text = {
+  showImage: "Show image",
+  hideImage: "Hide image",
+};
+
 app.toggleImage = function (targetId) {
   var elem = $("#" + targetId);
   var isVisible = $(elem).data("target-visible") == "1";
@@ -10,17 +15,46 @@ app.toggleImage = function (targetId) {
   if (isVisible) {
     // Hide the image
     $("#" + targetId + " .irt-image-img").hide();
-    $("#" + targetId + " .irt-toggle-image").text("Show image");
+    $("#" + targetId + " .irt-toggle-image").text(app.text.showImage);
     $(elem).data("target-visible", "0");
+    $(elem).addClass("d-print-none");
   } else {
     // Show the image
     $("#" + targetId + " .irt-image-img").show();
-    $("#" + targetId + " .irt-toggle-image").text("Hide image");
+    $("#" + targetId + " .irt-toggle-image").text(app.text.hideImage);
     $(elem).data("target-visible", "1");
+    $(elem).removeClass("d-print-none");
   }
 };
 
+app.hideAllImages = function () {
+  $(".irt-image .irt-image-img").hide();
+  $(".irt-image .irt-toggle-image").text(app.text.showImage);
+  $(".irt-image").data("target-visible", "0");
+  $(".irt-image").addClass("d-print-none");
+  Cookies.set("showImages", "0", { expires: 30 });
+};
+
+app.showAllImages = function () {
+  $(".irt-image .irt-image-img").show();
+  $(".irt-image .irt-toggle-image").text(app.text.hideImage);
+  $(".irt-image").data("target-visible", "1");
+  $(".irt-image").removeClass("d-print-none");
+  Cookies.set("showImages", "1", { expires: 30 });
+};
+
 $(function () {
+  // Initial page load config
+  if (Cookies.get("showImages") === undefined) {
+    // First run, so hide all images and set the cookie to 0
+    // (Cookies are set inside the hideAllImages & showAllImages functions)
+    app.hideAllImages();
+  } else if (Cookies.get("showImages") === "0") {
+    app.hideAllImages();
+  }
+  // Otherwise, show the images (default HTML behaviour)
+
+  // Button click assignments:
   $(".irt-print-current").on("click", function (e) {
     window.print();
   });
@@ -29,5 +63,13 @@ $(function () {
     var elem = $(e.currentTarget);
     var target = $(elem).data("target-img");
     app.toggleImage(target);
+  });
+
+  $(".irt-hide-all-images").on("click", function (e) {
+    app.hideAllImages();
+  });
+
+  $(".irt-show-all-images").on("click", function (e) {
+    app.showAllImages();
   });
 });
